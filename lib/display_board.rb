db2b501be4e2bf81bd9b6f2236dea7868d07f57f
Jared Chamberlain <jchamberlain909@gmail.com>
2018-04-30 17:05:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-put "   |   |   "
-put "   |   |   "
-put "-----------"
-put "   |   |   "
-put "   |   |   "
-put "-----------"
-put "   |   |   "
-put "   |   |   "
+puts "   |   |   "
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "   |   |   "
 
 end
 
